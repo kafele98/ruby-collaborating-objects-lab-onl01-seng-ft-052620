7768af2e0c9b5988bdaa261d88f967f1cadefdc9
Zoe Keller <keller.zoe98@gmail.com>
2020-06-15 13:50:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,11 @@ require 'pry'
 class Song
   attr_accessor :name, :artist
 
+  @@all= [] 
+  
   def initialize(name)
     @name = name
+    @@all << self 
   end
 
   def self.new_by_filename(file)
